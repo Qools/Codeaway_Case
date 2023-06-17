8a0444f8a80dabc2d4332d3f0c9244f4819b37cf
Denys Ostapenko <denys.ost92@gmail.com>
2023-06-18 01:19:23 +0300

    build ui

--- Assets/_Project/Scripts/Building/BuildManager.cs
+++ Assets/_Project/Scripts/Building/BuildManager.cs
@@ -26,31 +26,15 @@ public class BuildManager : Singleton<BuildManager>
 
     public void BuildTurretOn(Node node)
     {
-        if (turretToBuild == null)
+        if (PlayerStats.Money < turretToBuild.price)
         {
-            if (PlayerStats.Money < standardTurretPrefab.price)
-            {
-                return;
-            }
-
-            PlayerStats.Money -= standardTurretPrefab.price;
-
-            GameObject turret = Instantiate(standardTurretPrefab.turretPrefab, node.GetOffsetPosition(), Quaternion.identity);
-            node.turret = turret;
+            return;
         }
 
-        else
-        {
-            if (PlayerStats.Money < turretToBuild.price)
-            {
-                return;
-            }
+        PlayerStats.Money -= turretToBuild.price;
 
-            PlayerStats.Money -= turretToBuild.price;
-
-            GameObject turret = Instantiate(turretToBuild.turretPrefab, node.GetOffsetPosition(), Quaternion.identity);
-            node.turret = turret;
-        }
+        GameObject turret = Instantiate(turretToBuild.turretPrefab, node.GetOffsetPosition(), Quaternion.identity);
+        node.turret = turret;
     }
 
     public void SetTurretToBuild(TurretAttributes _turret)
