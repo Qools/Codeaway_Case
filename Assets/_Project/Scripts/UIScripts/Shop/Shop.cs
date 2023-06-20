13b408e725110744c60c2710fe72b092466348ef
Denys Ostapenko <denys.ost92@gmail.com>
2023-06-20 18:08:57 +0300

    new turrets, clean up

--- Assets/_Project/Scripts/UIScripts/Shop/Shop.cs
+++ Assets/_Project/Scripts/UIScripts/Shop/Shop.cs
@@ -1,28 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 
 public class Shop : MonoBehaviour
 {
-    public List<TurretBlueprint> turretAttributes = new List<TurretBlueprint>();
+    public List<TurretBlueprint> turretBlueprint = new List<TurretBlueprint>();
     [SerializeField] private List<TextMeshProUGUI> priceTexts = new List<TextMeshProUGUI>();
+    [SerializeField] private List<Image> turretIcons = new List<Image>(); 
 
     private void Start()
     {
         SetPriceTexts();
+
+        SetTurretIcons();
+    }
+
+    private void SetTurretIcons()
+    {
+        for (int i = 0; i < turretIcons.Count; i++)
+        {
+            turretIcons[i].sprite = turretBlueprint[i].turretIcon;
+        }
     }
 
     private void SetPriceTexts()
     {
         for (int i = 0; i < priceTexts.Count; i++)
         {
-            priceTexts[i].text = "$" + turretAttributes[i].price.ToString();
+            priceTexts[i].text = "$" + turretBlueprint[i].price.ToString();
         }
     }
 
     public void PurchaseTurret(int index)
     {
-        BuildManager.Instance.SetTurretToBuild(turretAttributes[index]);
+        BuildManager.Instance.SetTurretToBuild(turretBlueprint[index]);
     }
 }
